ac9e184e9030706ef3ef2eeaa5e0ee7ebe3eee1b
Aditya Ramesh <ramesh.aditya@gmail.com>
2017-09-18 17:27:56 +0530

    Upload DSU

---
+++ Disjoint-set-union.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int par[10];
+
+//Union-Find in 2 lines
+int find(int x){par[x]==x?x:find(par[x]);}
+int merge(int x,int y){par[find(x)]=find(y);}
+
+int main(){
+	for(int i=0;i<10;i++)par[i]=i;
+	merge(3,4);
+	merge(4,5);
+	for(int i=0;i<10;i++)printf("%d ",find(i));
+}
