23be1d4d55a051307594b4e8031758869cd28bb2
alexnewman312 <39803936+alexnewman312@users.noreply.github.com>
2020-10-01 18:57:20 +0530

    Update Disjoint-set-union.cpp

--- Disjoint-set-union.cpp
+++ Disjoint-set-union.cpp
@@ -6,6 +6,9 @@ int par[10];
 int find(int x){par[x]==x?x:find(par[x]);}
 int merge(int x,int y){par[find(x)]=find(y);}
 
+int find(int x){par[x]==x?x:find(par[x]);}
+int merge(int x, int y){par[find(x)] = find(y);}
+
 int main(){
 	for(int i=0;i<10;i++)par[i]=i;
 	merge(3,4);
